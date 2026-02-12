8ddc1bb0c5d6c1050c2bc4376fbca75f5d40cdd6
Dillon Wilson <greasyRooster1@gmail.com>
2026-02-12 13:29:03 -0800

    all the other static checks

--- src/lexer/tokens.rs
+++ src/lexer/tokens.rs
@@ -35,3 +35,63 @@ impl TokenRule for LoopRule {
         Token::Loop
     }
 }
+
+impl TokenRule for TypeSeparatorRule {
+    fn check(&self, string: String) -> bool {
+        string == ":"
+    }
+
+    fn get_token(&self, string: String) -> Token {
+        Token::TypeSeparator
+    }
+}
+
+impl TokenRule for RangeSeparatorRule {
+    fn check(&self, string: String) -> bool {
+        string == ".."
+    }
+
+    fn get_token(&self, string: String) -> Token {
+        Token::RangeSeparator
+    }
+}
+
+impl TokenRule for OpenParamRule {
+    fn check(&self, string: String) -> bool {
+        string == "("
+    }
+
+    fn get_token(&self, string: String) -> Token {
+        Token::OpenParam
+    }
+}
+
+impl TokenRule for CloseParamRule {
+    fn check(&self, string: String) -> bool {
+        string == ")"
+    }
+
+    fn get_token(&self, string: String) -> Token {
+        Token::CloseParam
+    }
+}
+
+impl TokenRule for OpenBracketRule {
+    fn check(&self, string: String) -> bool {
+        string == "{"
+    }
+
+    fn get_token(&self, string: String) -> Token {
+        Token::OpenBracket
+    }
+}
+
+impl TokenRule for CloseBracketRule {
+    fn check(&self, string: String) -> bool {
+        string == "}"
+    }
+
+    fn get_token(&self, string: String) -> Token {
+        Token::CloseBracket
+    }
+}
