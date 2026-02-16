e264478b510cada450254157924fd476987b985e
Dillon Wilson <greasyRooster1@gmail.com>
2026-02-16 15:27:17 -0800

    pass str ref

--- src/lexer/tokens.rs
+++ src/lexer/tokens.rs
@@ -16,108 +16,108 @@ pub struct OpenParamRule;
 pub struct CloseParamRule;
 pub struct OpenBracketRule;
 pub struct CloseBracketRule;
-
+clone()
 impl TokenRule for FuncRule {
-    fn check(&self, string: String) -> bool {
+    fn check(&self, string: &String) -> bool {
         string == "#"
     }
 
-    fn get_token(&self, string: String) -> Token {
+    fn get_token(&self, string: &String) -> Token {
         Token::Func
     }
 }
 
 impl TokenRule for LoopRule {
-    fn check(&self, string: String) -> bool {
+    fn check(&self, string: &String) -> bool {
         string == "@"
     }
 
-    fn get_token(&self, string: String) -> Token {
+    fn get_token(&self, string: &String) -> Token {
         Token::Loop
     }
 }
 
 impl TokenRule for TypeSeparatorRule {
-    fn check(&self, string: String) -> bool {
+    fn check(&self, string: &String) -> bool {
         string == ":"
     }
 
-    fn get_token(&self, string: String) -> Token {
+    fn get_token(&self, string: &String) -> Token {
         Token::TypeSeparator
     }
 }
 
 impl TokenRule for RangeSeparatorRule {
-    fn check(&self, string: String) -> bool {
+    fn check(&self, string: &String) -> bool {
         string == ".."
     }
 
-    fn get_token(&self, string: String) -> Token {
+    fn get_token(&self, string: &String) -> Token {
         Token::RangeSeparator
     }
 }
 
 impl TokenRule for OpenParamRule {
-    fn check(&self, string: String) -> bool {
+    fn check(&self, string: &String) -> bool {
         string == "("
     }
 
-    fn get_token(&self, string: String) -> Token {
+    fn get_token(&self, string: &String) -> Token {
         Token::OpenParam
     }
 }
 
 impl TokenRule for CloseParamRule {
-    fn check(&self, string: String) -> bool {
+    fn check(&self, string: &String) -> bool {
         string == ")"
     }
 
-    fn get_token(&self, string: String) -> Token {
+    fn get_token(&self, string: &String) -> Token {
         Token::CloseParam
     }
 }
 
 impl TokenRule for OpenBracketRule {
-    fn check(&self, string: String) -> bool {
+    fn check(&self, string: &String) -> bool {
         string == "{"
     }
 
-    fn get_token(&self, string: String) -> Token {
+    fn get_token(&self, string: &String) -> Token {
         Token::OpenBracket
     }
 }
 
 
 impl TokenRule for NameRule {
-    fn check(&self, string: String) -> bool {
+    fn check(&self, string: &String) -> bool {
         string.starts_with(char::is_alphabetic) &&
         string.chars().all(char::is_alphanumeric)
     }
 
-    fn get_token(&self, string: String) -> Token {
-        Token::Name(string)
+    fn get_token(&self, string: &String) -> Token {
+        Token::Name(*string)
     }
 }
 
 impl TokenRule for StringLiteralRule {
-    fn check(&self, string: String) -> bool {
+    fn check(&self, string: &String) -> bool {
         string.starts_with("\"") &&
         string.ends_with("\"")
     }
 
-    fn get_token(&self, string: String) -> Token {
-        Token::Name(string)
+    fn get_token(&self, string: &String) -> Token {
+        Token::Name(*string)
     }
 }
 
 impl TokenRule for NumberLiteralRule {
-    fn check(&self, string: String) -> bool {
+    fn check(&self, string: &String) -> bool {
         string.parse::<f64>().is_ok() ||
         string.parse::<i128>().is_ok() ||
         string.parse::<u128>().is_ok()
     }
 
-    fn get_token(&self, string: String) -> Token {
+    fn get_token(&self, string: &String) -> Token {
         let mut assumption;
         let value = string.parse::<f64>();
         if string.parse::<i32>().is_ok(){
@@ -127,6 +127,6 @@ impl TokenRule for NumberLiteralRule {
         }else{
             assumption = NumberLiteralAssumptions::ExplicitRequired
         }
-        Token::NumberLiteral(string,assumption)
+        Token::NumberLiteral(*string, assumption)
     }
 }
