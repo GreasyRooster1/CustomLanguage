8c6948d46e7c6bbd9f0d27b9925d92f006b28caf
Dillon Wilson <greasyRooster1@gmail.com>
2026-02-16 15:09:57 -0800

    parse

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,9 +1,21 @@
 use crate::lexer::{Token, TokenRule};
+use crate::lexer::tokens::*;
 
-fn matching_tokens(string:String,rules:Vec<Box<dyn TokenRule>>) -> Vec<Token> {
+fn get_matching_tokens(string:String, rules:Vec<Box<dyn TokenRule>>) -> Vec<Token> {
     let mut tokens = vec![];
     
     
     
     tokens
+}
+
+fn parse(){
+    let rules = alloc_rules();
+
+}
+
+fn alloc_rules() -> Vec<Box<dyn TokenRule>>{
+    vec![
+        Box::new(FuncRule),
+    ]
 }
