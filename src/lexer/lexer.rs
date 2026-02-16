878ba83f0e890084a7774ab68d959b19840cf857
Dillon Wilson <greasyRooster1@gmail.com>
2026-02-16 15:34:11 -0800

    alloc all rules

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,4 +1,6 @@
+use std::intrinsics::mir::Return;
 use crate::lexer::{Token, TokenRule};
+use crate::lexer::Token::{OpenParam, TypeSeparator};
 use crate::lexer::tokens::*;
 
 pub(crate) fn get_matching_tokens(string:String, rules:Vec<Box<dyn TokenRule>>) -> Vec<Token> {
@@ -21,5 +23,19 @@ pub(crate) fn parse(){
 pub(crate) fn alloc_rules() -> Vec<Box<dyn TokenRule>>{
     vec![
         Box::new(FuncRule),
+        Box::new(LoopRule),
+
+        Box::new(NumberLiteralRule),
+        Box::new(StringLiteralRule),
+        Box::new(NameRule),
+
+        Box::new(TypeSeparatorRule),
+        Box::new(RangeSeparatorRule),
+
+        Box::new(OpenParamRule),
+        Box::new(CloseParamRule),
+        Box::new(OpenBracketRule),
+        Box::new(CloseBracketRule),
+
     ]
 }
