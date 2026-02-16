7d64b685befcb340bba8e7eef308e3a93202b317
Dillon Wilson <greasyRooster1@gmail.com>
2026-02-16 15:45:08 -0800

    ufloat

--- src/lexer/tests.rs
+++ src/lexer/tests.rs
@@ -2,8 +2,10 @@
 mod tests {
     use std::cmp::PartialEq;
     use std::mem;
+    use rand::random;
     use crate::lexer::lexer::*;
     use crate::lexer::{Token, TokenRule};
+    use crate::lexer::NumberLiteralAssumptions::Float;
 
     fn check_single_token_parse(string:String, rules:&Vec<Box<dyn TokenRule>>, token: Token) -> bool{
         let t = get_matching_tokens(string,rules);
@@ -24,5 +26,14 @@ mod tests {
         assert!(check_single_token_parse(")".to_string(),&rules,Token::CloseParam));
         assert!(check_single_token_parse("{".to_string(),&rules,Token::OpenBracket));
         assert!(check_single_token_parse("}".to_string(),&rules,Token::CloseBracket));
+    }
+
+    #[test]
+    fn test_number_literal_ufloat_token() {
+        let rules = alloc_rules();
+        for i in 0..1000{
+            let num = random::<f64>().to_string();
+            assert!(check_single_token_parse(num.clone(),&rules,Token::NumberLiteral(num,Float)))
+        }
     }
 }
